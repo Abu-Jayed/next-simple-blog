25093ec5b33667f37f68dd988e2ebe37126848ed
Abu Jayed <aj.webdevlopment@gmail.com>
2023-10-22 12:42:08 +0600

    portfolio detail page completed.

--- src/app/portfolio/[ctgName]/page.jsx
+++ src/app/portfolio/[ctgName]/page.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import Button from "../../../components/Button/Button";
+import { items } from "./data.js";
+import styles from "./page.module.css";
 
-const Category = () => {
+const getData = (cat) => {
+  const data = items[cat];
+  console.log("data",data);
+
+  if (data) {
+    return data;
+  }
+
+  return notFound();
+};
+
+const Category = ({ params }) => {
+  console.log(params.ctgName);
+  const data = getData(params.ctgName);
   return (
-    <div>
-      Category
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text="See More" url="#" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image className={styles.img} fill={true} src={item.image} alt="" />
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
