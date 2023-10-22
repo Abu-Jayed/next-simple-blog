075f5f0669367e7f15ccc2f0a605a23566638ff0
Abu Jayed <aj.webdevlopment@gmail.com>
2023-10-22 06:48:34 +0600

    image added to footer components.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   const { container } = styles;
   return (
     <div className={container}>
       <div>&copy;2024, Abu Jayed. All rights reserved.</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image className="opacity-[0.6] cursor-pointer" src={"/facebook.png"} width={15} height={15} alt="Abu Jayed facebook"></Image>
+        <Image className="opacity-[0.6] cursor-pointer" src={"/instagram.png"} width={15} height={15} alt="Abu Jayed facebook"></Image>
+        <Image className="opacity-[0.6] cursor-pointer" src={"/tiktok.png"} width={15} height={15} alt="Abu Jayed facebook"></Image>
+        <Image className="opacity-[0.6] cursor-pointer" src={"/youtube.png"} width={15} height={15} alt="Abu Jayed facebook"></Image>
+      </div>
     </div>
   );
 };
